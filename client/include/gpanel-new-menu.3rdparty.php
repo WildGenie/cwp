beabda5ba8bfedbbaaffaf82e565cb7cdeb695a0
Gogo <gogo1207@users.noreply.github.com>
2017-10-25 01:32:05 +0200

    Update gpanel-new-menu.3rdparty.php

--- client/include/gpanel-new-menu.3rdparty.php
+++ client/include/gpanel-new-menu.3rdparty.php
@@ -1,10 +1,7 @@
 <!-- START-GamePanel -->
 <script type="text/javascript">
 	$(document).ready(function() {
-		var newGpButtons = ''
-		+' <li>'
-		+' <a href="index.php?module=Game_Panel"><span aria-hidden="true" class="icon16 wpzoom-gamepad-2" style="padding-right: 8px;"></span>Gamepanel</a>'
-		+'</li>';
+		var newGpButtons = '<li><a href="index.php?module=Game_Panel"><span aria-hidden="true" class="icon16 wpzoom-gamepad-2" style="padding-right: 8px;"></span>Gamepanel</a></li>';
 		$(".mainnav > ul").append(newGpButtons);
 	});
 </script>
