cde97a9a53a667850f5afeab75966939e4aa4deb
Gogo <gogo1207@users.noreply.github.com>
2017-10-27 18:23:04 +0200

    Create add-custom-navmenu.3rdparty.php

---
+++ hooks/add-custom-navmenu.3rdparty.php
@@ -0,0 +1,29 @@
+<!-- This will create new button with submenu at the CWP's top nav menu -->
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		var newNavButtonsList = '';
+
+		/* Add SubButtons here, as many as you want :D */
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 1</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 2</a></li>';
+		newNavButtonsList += '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 3</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 4</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 5</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 6</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 7</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 8</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 9</a></li>';
+		newNavButtonsList += '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 10</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 11</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 12</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 13</a></li>';
+		newNavButtonsList	+= '<li><a class="navSub" href="#"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub Menu 14</a></li>';
+
+		/* not recommended to edit if you don't know what you are doing */
+		var newNavButtonsStyle = '<style type="text/css">ul.navSub {background-color: #f4f4f4;border-right: 1px solid #bfbfbf;border-left: 1px solid #bfbfbf;border-bottom: 1px solid #bfbfbf;padding-left: 5px;list-style: none;padding-top: 5px;position: absolute;min-width: 100%;min-height: 100%;display: none;}a.newNavButtons:hover + ul.navSub {display:block;}ul.navSub:hover {display:block;}a.navSub:hover {background: none !important;box-shadow: none !important;width: 150px !important;position: relative;}</style>';
+		var newNavButtons = '<li><a a href="#" class="newNavButtons"><span aria-hidden="true" class="icon16 wpzoom-star" style="padding-right: 10px;"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="navSub">'+newNavButtonsList+'</ul></li>';
+		$(newNavButtonsStyle).appendTo($('head'));
+		$(newNavButtons).appendTo($('.navbar-nav'));
+	});
+</script>
