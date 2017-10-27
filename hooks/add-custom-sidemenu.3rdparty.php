283c7f3c5a7cdf2286f9d8be7ae9cb2dff189fd3
Gogo <gogo1207@users.noreply.github.com>
2017-10-27 19:06:16 +0200

    Update add-custom-sidemenu.3rdparty.php

--- hooks/add-custom-sidemenu.3rdparty.php
+++ hooks/add-custom-sidemenu.3rdparty.php
@@ -1,35 +1,32 @@
 <!-- Example 1: Including submenus! -->
 <script type="text/javascript">
-	var newSideButtonsList = ''; // leave this empty
+	$(document).ready(function() {
+		var newSideButtonsList = ''; // leave this empty
 
-	var newSideButtonsName = 'Custom Menu'; // rename main button as you want
+		var newSideButtonsName = 'Custom Menu'; // rename main button as you want
 
-	/* Add SubButtons here, as many as you want :D */
-	newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
-	newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
-	newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+		/* Add SubButtons here, as many as you want :D */
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
 
-	var newButtons = '<li><a href="#" class="hasUl"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="sub">'+newSideButtonsList+'</ul></li>';
-	$(newButtons).appendTo($('.mainnav > ul'));
+		var newButtons = '<li><a href="#" class="hasUl"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="sub">'+newSideButtonsList+'</ul></li>';
+		$(newButtons).appendTo($('.mainnav > ul'));
+	});
+	
 </script>
 
 
 <!-- Example 2: Without submenus! -->
 <script type="text/javascript">
-	var newButtons = ''; // leave empty
-
-	/* Add Buttons here, as many as you want :D */
-	newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 2</a></li>';
-	newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 3</a></li>';
-	newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 4</a></li>';
-
-	$(newButtons).appendTo($('.mainnav > ul'));
-</script>
-
-
-<!-- If there is any problem with printing buttons try to wrap buttons like this -->
-<script type="text/javascript">
 	$(document).ready(function() {
-		/* buttons code here */
+		var newButtons = ''; // leave empty
+
+		/* Add Buttons here, as many as you want :D */
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 2</a></li>';
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 3</a></li>';
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 4</a></li>';
+
+		$(newButtons).appendTo($('.mainnav > ul'));
 	});
 </script>
