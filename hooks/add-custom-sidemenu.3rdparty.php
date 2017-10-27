4c85f66a0fb759227598b349411115f690f9d2fc
Gogo <gogo1207@users.noreply.github.com>
2017-10-27 18:45:01 +0200

    Update add-custom-sidemenu.3rdparty.php

--- hooks/add-custom-sidemenu.3rdparty.php
+++ hooks/add-custom-sidemenu.3rdparty.php
@@ -11,7 +11,7 @@
 		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
 
 		var newButtons = '<li><a href="#" class="hasUl"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="sub">'+newSideButtonsList+'</ul></li>';
-		$(".mainnav > ul").append(newButtons);
+		$(newButtons).appendTo($('.mainnav > ul'));
 	});
 </script>
 
@@ -26,6 +26,6 @@
 		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 3</a></li>';
 		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 4</a></li>';
 
-		$(".mainnav > ul").append(newButtons);
+		$(newButtons).appendTo($('.mainnav > ul'));
 	});
 </script>
