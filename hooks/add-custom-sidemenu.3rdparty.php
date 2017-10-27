445a26fbe7919fe0f9622ab6236149bc77ef46ac
Gogo <gogo1207@users.noreply.github.com>
2017-10-27 18:42:50 +0200

    Update add-custom-sidemenu.3rdparty.php

--- hooks/add-custom-sidemenu.3rdparty.php
+++ hooks/add-custom-sidemenu.3rdparty.php
@@ -1,15 +1,16 @@
 <!-- Example 1: Including submenus! -->
 <script type="text/javascript">
 	$(document).ready(function() {
-		var newButtons = ''
-		+' <li>'
-		+' <a href="#" class="hasUl"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a>'
-		+'	<ul class="sub">'
-		+'		<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>'
-		+'		<li><a href="#2"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 2</a></li>'
-		+'		<li><a href="#3"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 3</a></li>'
-		+'	</ul>'
-		+'</li>';
+		var newSideButtonsList = ''; // leave this empty
+
+		var newSideButtonsName = 'Custom Menu'; // rename main button as you want
+
+		/* Add SubButtons here, as many as you want :D */
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+		newSideButtonsList += '<li><a href="#1"><span class="icon16 icomoon-icon-arrow-right-3"></span>Sub 1</a></li>';
+
+		var newButtons = '<li><a href="#" class="hasUl"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="sub">'+newSideButtonsList+'</ul></li>';
 		$(".mainnav > ul").append(newButtons);
 	});
 </script>
@@ -18,11 +19,11 @@
 <!-- Example 2: Without submenus! -->
 <script type="text/javascript">
 	$(document).ready(function() {
-		var newButtons = ''
-		+'<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 2</a></li>'
-		+'<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 3</a></li>'
-		+'<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 4</a></li>'
-		;
+		var newButtons = ''; // leave empty
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 2</a></li>';
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 3</a></li>';
+		newButtons +='<li><a href="#your_link_here"><span aria-hidden="true" class="icon16 icomoon-icon-question"></span>Custom Menu 4</a></li>';
+
 		$(".mainnav > ul").append(newButtons);
 	});
 </script>
