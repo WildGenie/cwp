1719880a502ad7931c14d3692643e8e4ea1182fe
Gogo <gogo1207@users.noreply.github.com>
2017-10-27 18:35:59 +0200

    Update add-custom-navmenu.3rdparty.php

--- hooks/add-custom-navmenu.3rdparty.php
+++ hooks/add-custom-navmenu.3rdparty.php
@@ -1,3 +1,4 @@
+<!-- Example 1: With submenu -->
 <script type="text/javascript">
 	$(document).ready(function() {
 		var newNavButtonsList = ''; // leave this empty
@@ -22,8 +23,20 @@
 
 		/* not recommended to edit if you don't know what you are doing */
 		var newNavButtonsStyle = '<style type="text/css">ul.navSub {background-color: #f4f4f4;border-right: 1px solid #bfbfbf;border-left: 1px solid #bfbfbf;border-bottom: 1px solid #bfbfbf;padding-left: 5px;list-style: none;padding-top: 5px;position: absolute;min-width: 100%;min-height: 100%;display: none;}a.newNavButtons:hover + ul.navSub {display:block;}ul.navSub:hover {display:block;}a.navSub:hover {background: none !important;box-shadow: none !important;width: 150px !important;position: relative;}</style>';
-		var newNavButtons = '<li><a a href="#" class="newNavButtons"><span aria-hidden="true" class="icon16 wpzoom-star" style="padding-right: 10px;"></span>'+newNavButtonsName+'<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="navSub">'+newNavButtonsList+'</ul></li>';
+		var newNavButtons = '<li><a a href="#" class="newNavButtons"><span aria-hidden="true" class="icon16 wpzoom-star"></span>'+newNavButtonsName+'<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a><ul class="navSub">'+newNavButtonsList+'</ul></li>';
 		$(newNavButtonsStyle).appendTo($('head'));
 		$(newNavButtons).appendTo($('.navbar-nav'));
+	});
+</script>
+
+<!-- Example 2: Without submenus -->
+<script type="text/javascript">
+	$(document).ready(function() {
+		var newNavButton = ''; // leave this empty
+
+		newNavButton += '<li><a target="_blank" href="#"><span class="icon16 wpzoom-star"></span> <span class="txt">Custom Button 1</span></a></li>';
+		newNavButton += '<li><a target="_blank" href="#"><span class="icon16 wpzoom-star"></span> <span class="txt">Custom Button 1</span></a></li>';
+
+		$(newNavButton).appendTo($('.navbar-nav'));
 	});
 </script>
