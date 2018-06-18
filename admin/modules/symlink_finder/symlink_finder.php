6eeb0128fed4fc6cf97337287e92b5a906e51b0f
Gogo <gogo1207@users.noreply.github.com>
2018-06-18 23:48:37 +0200

    recoded
    Added DataTable and jQuery, Removed unused flusher. Added set_time_limit so script can work on slower machines.

--- admin/modules/symlink_finder/symlink_finder.php
+++ admin/modules/symlink_finder/symlink_finder.php
@@ -2,30 +2,69 @@
 /* Symlinks Finder for Centos-WebPanel by Gogo */
 if(!isset($include_path) OR preg_match("/".basename(__FILE__)."/i",$_SERVER['REQUEST_URI'])){exit('Direct access not permitted!');}
 
-function doLines($lines){
-  $out = null;
-  foreach($lines as $line){
-    $out .= $line.PHP_EOL;
-  }
-  return $out;
-}
-
-function flushGarbage(){
-  for ($i=0; $i < 100; $i++) {
-    echo '<!-- This is just garbage content for flusher... -->';
-  }
-}
-
 function _findSymLinks(){
+    set_time_limit(180);
     exec('cd / ;'.PHP_EOL.' find /* -type l -ls;'.PHP_EOL, $lines);
-    echo 'Found Symlinks: '.count($lines);
-    echo '<pre>'.doLines($lines).'</pre>';
+    echo '<script>var symlinks = '.((!empty($lines)) ? json_encode($lines) : '').';</script>';
 }
 
-echo '<h3>All Symlinks available:</h3><br><br>';
-_findSymLinks();
+if(!empty($_POST['symlinkfinder_search']) AND $_POST['symlinkfinder_search'] == "search") {
+    _findSymLinks();
+} else {
+    echo '<p style="color:red;font-weight:600;">Searching for all Symlinks can take a while. Please be patient while searching!</p>';
+    echo '<script>var symlinks = \'\';</script>';
+}
 
-/* be fair and don't remove next line, thank you */
-echo '<br><br><small>Donate <a href="https://paypal.me/gogo1207"><i aria-hidden="true" class="icomoon-icon-paypal"></i></a> | &copy;2017 <a style="color: #353535;" href="https://goran-margetic.iz.hr">GM</a></small>';
-
+echo '<form method="post">
+    <input type="hidden" class="form-control" name="symlinkfinder_search" value="search">
+    <button type="submit" class="btn btn-success">'.((!empty($_POST['symlinkfinder_search'])) ? 'Search again?' : 'Search').'</button>
+</form><br>';
 ?>
+
+<link rel="stylesheet" href="design/3rdparty/datatables/css/jquery.dataTables.min.css">
+<div class="content noPad clearfix">
+    <table id="symlinks" cellpadding="0" cellspacing="0" border="0" class="responsive dynamicTable display table table-bordered" width="100%">
+        <thead>
+            <tr class='evenrowcolor'></tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
+<script type="text/javascript" src="design/3rdparty/datatables/js/jquery.dataTables.min.js"></script>
+<style>
+  .col-id{width: 30px !important;}
+</style>
+<script type="text/javascript">
+  var jq = $.noConflict();
+  jq(document).ready(function($){
+    if(symlinks != ''){
+        i = 1;
+        var table = $('#symlinks').DataTable({
+            "aaSorting": [],
+            "rowReorder": false,
+            "sPaginationType": "full_numbers",
+            "bPaginate": true,
+            "bJQueryUI": false,
+            "bLengthChange": true,
+            "bFilter": true,
+            "bSort": true,
+            "bInfo": true,
+            "bAutoWidth": true,
+            "bProcessing": true,
+            "iDisplayLength": 25,
+            "deferRender": true,
+            columns: [
+              { title: '#', class: 'col-id'},
+              { title: 'Symlink'}
+            ]
+          });
+          jq(symlinks).each(function( index ) {
+            table.row.add([i++, symlinks[index]]);
+          });
+          table.draw();
+      }
+});
+</script>
+
+<!-- be fair and don't remove next line, thank you -->
+<br><br><small>Donate <a href="https://paypal.me/gogo1207"><i aria-hidden="true" class="icomoon-icon-paypal"></i></a> | &copy;2017 <a style="color: #353535;" href="https://goran-margetic.iz.hr">GM</a></small>
